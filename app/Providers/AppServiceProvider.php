3fe94c1832e1d989e2efb71a046ad50209c1239b
alaya-dev <3leya21@gmail.com>
2026-07-23 02:26:55 +0100

    Fix import statement for ServiceProvider in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\ServiceProvider;
 use App\Domain\Catalog\Models\Category;
 use App\Domain\Checkout\Services\ShippingCalculator;
 use App\Domain\Content\Models\StaticPage;
